d4835f23aed11b6a8728ca2924aed80501106baf
Hemant Suman <hemant.suman@a3logics.in>
2020-02-27 14:26:09 +0530

    create script during installation

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var shopify = require('./routes/shopify');
 
 var app = express();
-
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
