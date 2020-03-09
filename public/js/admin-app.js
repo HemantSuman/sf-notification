e894b43de9a1cb6af87fdf73da9d20d47253a1f8
Hemant Suman <hemant.suman@a3logics.in>
2020-03-09 12:44:46 +0530

    add notification div front end

--- public/js/admin-app.js
+++ public/js/admin-app.js
@@ -86,7 +86,7 @@ jQuery(document).ready(function(){
 		};
 
 		$.ajax({
-			url: '/shopify/save-settings',
+			url: '/sf-notification/shopify/save-settings',
 			data: formData,
 			dataType: "json",
 			method: "POST",
