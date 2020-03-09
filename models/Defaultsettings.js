8b27fa73ccdc8f1c38fa19ef88a1e19d6820cba7
Hemant Suman <hemant.suman@a3logics.in>
2020-03-09 15:51:38 +0530

    new model draft settings

---
+++ models/Defaultsettings.js
@@ -0,0 +1,318 @@
+module.exports = function (sequelize, DataTypes) {
+    const Op = sequelize.Op;
+    var myModel = sequelize.define("Defaultsettings",
+        {
+            // id: {
+            //     type: DataTypes.INTEGER,
+            //     // validate: {
+            //     //     notEmpty: {
+            //     //         msg: i18n_Validation.__('required')
+            //     //     },
+            //     // }
+            // },
+            EnableDisableNotificationBar: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            BackgroundColor: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            title: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            TitleFontFamily: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            TitleFontsize: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            TitleTextColor: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            TitleBGColor: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            textarea1: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            MessageFontFamily: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            MessageFontSize: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            MessageTextColor: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            MessageBGColor: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            ButtonLabel: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            ButtonURL: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            ButtonFontFamily: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            ButtonFontsize: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            BtnTextColor: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            BtnBGColor: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            Buttonstyles: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            BtnButtonRadius: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            EnableDisableCloseButton: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            Icons: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            CloseBtnTextColor: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            CloseBtnBgColor: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            CloseButtonType: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            ButtonRadius: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            EnableDisableCountdownSection: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            StartTime: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            EndTime: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            CountdownType: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            CountdownTextColor: {
+                type: DataTypes.STRING,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+            status: {
+                type: DataTypes.INTEGER,
+                // validate: {
+                //     notEmpty: {
+                //         msg: i18n_Validation.__('required')
+                //     },
+                // }
+            },            
+        },
+        {
+            tableName: 'default_settings',
+        }
+    );
+    myModel.getAllValues = function (req, res) {
+
+        myModel.findAll({
+            // attributes: ['id'],
+            // where: { username: 'username' },
+            // where: {[Op.and]:req.where},
+        }).then(function (results) {
+            res(results);
+        });
+
+        // res('foo');
+    };
+    myModel.saveAllValues = function (req, res) {
+         myModel.create(req.body).then(function (results) {
+            results.status = 1;
+            res(results);
+        }).catch(function (err) {
+            console.log(err)
+            var errors = err;
+            errors.status = false;
+            res(errors);
+        });
+    };
+    myModel.deleteAllValues = function (req, res) {
+        myModel.destroy({where: req.where}).then(function (data) {
+            var results = {};
+            results.status = true;
+            res(results);
+        });
+    };
+    myModel.updateAllValues = function (req, res) {
+        myModel.update(req.body, {where: req.where}).then(function (data) {
+            var results = {};
+            results = req.body;
+            results.status = true;
+            res(results);
+        }).catch(function (err) {
+
+            var errors = err;
+            errors.status = false;
+            res(errors);
+        });
+    };
+    return myModel;
+};
--- routes/shopify.js
+++ routes/shopify.js
@@ -44,14 +44,17 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/test', (req, res) => {
-  req.body = {
-    shop_name: 'shop',
-    access_token: 'accessToken'
-  };
-  req.body.settings_val = {};
-  req.body.settings_val['title'] = 'dfgdfgfdgdf';
-  models['UserInfo'].saveAllValues(req, function (results1) {
-    console.log(results1);
+  // req.body = {
+  //   shop_name: 'shop',
+  //   access_token: 'accessToken'
+  // };
+  // req.body.settings_val = {};
+  // req.body.settings_val['title'] = 'dfgdfgfdgdf';
+  // models['UserInfo'].saveAllValues(req, function (results1) {
+  //   console.log(results1);
+  // });
+  models['Defaultsettings'].getAllValues(req, function (defaultSet) {
+    console.log('defaultSetdefaultSet', defaultSet[0].EnableDisableNotificationBar);
   });
 });
 
@@ -100,136 +103,143 @@ router.get('/callback', (req, res) => {
     request.post(accessTokenRequestUrl, { json: accessTokenPayload })
     .then((accessTokenResponse) => {
 
-      let accessToken = accessTokenResponse.access_token;
-      req.body = {
-                  shop_name: shop,
-                  access_token: accessToken
+      models['Defaultsettings'].getAllValues(req, function (defaultSet) {
+        console.log('defaultSetdefaultSet', defaultSet[0].EnableDisableNotificationBar);
+        let accessToken = accessTokenResponse.access_token;
+        req.body = {
+                    shop_name: shop,
+                    access_token: accessToken
+                  };
+        req.body.settings_val = {};
+        req.body.settings_val = defaultSet[0].EnableDisableNotificationBar;
+        req.body.settings_val.EnableDisableNotificationBar = null;
+
+        req.body.settings_draft_val = {};
+        req.body.settings_draft_val = defaultSet[0].EnableDisableNotificationBar;
+
+        models['UserInfo'].saveAllValues(req, function (results1) {
+          // res.redirect('/');
+          //request webhooks when app uninstalled
+          let new_request = {
+            "webhook": {
+              "topic": "app/uninstalled",
+              "address": forwardingAddress+"/shopify/uninstall-web-hooks",
+              "format": "json"
+            }
+          };
+          let shopRequestUrl = 'https://' + shop + '/admin/api/2019-10/webhooks.json';
+          let options = {
+            method: 'POST',
+            uri: shopRequestUrl,
+            json: true,
+            resolveWithFullResponse: true,//added this to view status code
+            headers: {
+                'X-Shopify-Access-Token': accessToken,
+                'content-type': 'application/json'
+            },
+            body: new_request
+          };
+          request.post(options)
+            .then((shopResponse) => {
+              req.body = {
+                webhook_id: shopResponse.body.webhook.id, 
+                topic: shopResponse.body.webhook.topic,
+                created_at: shopResponse.body.webhook.created_at,
+                address: shopResponse.body.webhook.address,
+              };
+              console.log('webhook save', req.body)
+              models['WebHook'].saveAllValues(req, function (results2) {
+                // res.redirect('/');
+
+                let new_request1 = {
+                  "recurring_application_charge": {
+                    "name": "New Plan Test 1111111",
+                    "price": 1.0,
+                    "return_url": forwardingAddress+"/shopify/recurring_application_charge_return?shop="+shop,
+                    "test": true,
+                    "trial_days":1
+                  }
                 };
-      req.body.settings_val = {};
-      // req.body.settings_val.EnableDisableNotificationBar = true;
-      req.body.settings_draft_val = {};
-      // req.body.settings_draft_val.EnableDisableNotificationBar = true;
-      models['UserInfo'].saveAllValues(req, function (results1) {
-        // res.redirect('/');
-        //request webhooks when app uninstalled
-        let new_request = {
-          "webhook": {
-            "topic": "app/uninstalled",
-            "address": forwardingAddress+"/shopify/uninstall-web-hooks",
-            "format": "json"
-          }
-        };
-        let shopRequestUrl = 'https://' + shop + '/admin/api/2019-10/webhooks.json';
-        let options = {
-          method: 'POST',
-          uri: shopRequestUrl,
-          json: true,
-          resolveWithFullResponse: true,//added this to view status code
-          headers: {
-              'X-Shopify-Access-Token': accessToken,
-              'content-type': 'application/json'
-          },
-          body: new_request
-        };
-        request.post(options)
-          .then((shopResponse) => {
-            req.body = {
-              webhook_id: shopResponse.body.webhook.id, 
-              topic: shopResponse.body.webhook.topic,
-              created_at: shopResponse.body.webhook.created_at,
-              address: shopResponse.body.webhook.address,
-            };
-            console.log('webhook save', req.body)
-            models['WebHook'].saveAllValues(req, function (results2) {
-              // res.redirect('/');
-
-              let new_request1 = {
-                "recurring_application_charge": {
-                  "name": "New Plan Test 1111111",
-                  "price": 1.0,
-                  "return_url": forwardingAddress+"/shopify/recurring_application_charge_return?shop="+shop,
-                  "test": true,
-                  "trial_days":1
-                }
-              };
-              let shopRequestUrl1 = 'https://' + shop + '/admin/api/2019-10/recurring_application_charges.json';
-              let options1 = {
-                method: 'POST',
-                uri: shopRequestUrl1,
-                json: true,
-                resolveWithFullResponse: true,//added this to view status code
-                headers: {
-                    'X-Shopify-Access-Token': accessToken,
-                    'content-type': 'application/json'
-                },
-                body: new_request1
-              };
-              request.post(options1)
-                .then((shopResponse1) => {
-                  
-                  req.body = { 
-                      charge_id: shopResponse1.body.recurring_application_charge.id,
-                      name: shopResponse1.body.recurring_application_charge.name,
-                      api_client_id: shopResponse1.body.recurring_application_charge.api_client_id,
-                      price: shopResponse1.body.recurring_application_charge.price,
-                      status: shopResponse1.body.recurring_application_charge.status,
-                      return_url:shopResponse1.body.recurring_application_charge.return_url,
-                      created_at: shopResponse1.body.recurring_application_charge.created_at,
-                      updated_at: shopResponse1.body.recurring_application_charge.updated_at,
-                      test: shopResponse1.body.recurring_application_charge.test,
-                      trial_days: shopResponse1.body.recurring_application_charge.trial_days,
-                      decorated_return_url:shopResponse1.body.recurring_application_charge.decorated_return_url,
-                      confirmation_url:shopResponse1.body.recurring_application_charge.confirmation_url,
-                    };
-                  req.where = {shop_name: shop};
-                  models['UserInfo'].updateAllValues(req, function (results1) {
+                let shopRequestUrl1 = 'https://' + shop + '/admin/api/2019-10/recurring_application_charges.json';
+                let options1 = {
+                  method: 'POST',
+                  uri: shopRequestUrl1,
+                  json: true,
+                  resolveWithFullResponse: true,//added this to view status code
+                  headers: {
+                      'X-Shopify-Access-Token': accessToken,
+                      'content-type': 'application/json'
+                  },
+                  body: new_request1
+                };
+                request.post(options1)
+                  .then((shopResponse1) => {
+                    
+                    req.body = { 
+                        charge_id: shopResponse1.body.recurring_application_charge.id,
+                        name: shopResponse1.body.recurring_application_charge.name,
+                        api_client_id: shopResponse1.body.recurring_application_charge.api_client_id,
+                        price: shopResponse1.body.recurring_application_charge.price,
+                        status: shopResponse1.body.recurring_application_charge.status,
+                        return_url:shopResponse1.body.recurring_application_charge.return_url,
+                        created_at: shopResponse1.body.recurring_application_charge.created_at,
+                        updated_at: shopResponse1.body.recurring_application_charge.updated_at,
+                        test: shopResponse1.body.recurring_application_charge.test,
+                        trial_days: shopResponse1.body.recurring_application_charge.trial_days,
+                        decorated_return_url:shopResponse1.body.recurring_application_charge.decorated_return_url,
+                        confirmation_url:shopResponse1.body.recurring_application_charge.confirmation_url,
+                      };
+                    req.where = {shop_name: shop};
+                    models['UserInfo'].updateAllValues(req, function (results1) {
 
 
-                    const shopRequestUrl3 = 'https://' + shop + '/admin/api/2019-10/script_tags.json';
-                    let new_script3 = {
-                      "script_tag": {
-                        "event": "onload",
-                        "src": forwardingAddress+"/js/script-tag.js"
-                      }
-                    };
+                      const shopRequestUrl3 = 'https://' + shop + '/admin/api/2019-10/script_tags.json';
+                      let new_script3 = {
+                        "script_tag": {
+                          "event": "onload",
+                          "src": forwardingAddress+"/js/script-tag.js"
+                        }
+                      };
 
-                    let options3 = {
-                        method: 'POST',
-                        uri: shopRequestUrl3,
-                        json: true,
-                        resolveWithFullResponse: true,//added this to view status code
-                        headers: {
-                            'X-Shopify-Access-Token': accessToken,
-                            'content-type': 'application/json'
-                        },
-                        body: new_script3
-                    };
+                      let options3 = {
+                          method: 'POST',
+                          uri: shopRequestUrl3,
+                          json: true,
+                          resolveWithFullResponse: true,//added this to view status code
+                          headers: {
+                              'X-Shopify-Access-Token': accessToken,
+                              'content-type': 'application/json'
+                          },
+                          body: new_script3
+                      };
 
-                    request.post(options3)
-                    .then((shopResponse3) => {
-                      console.log('shopResponse3',shopResponse3);
-                      res.redirect(shopResponse1.body.recurring_application_charge.confirmation_url); 
-                      // res.json(shopResponse3);
-                    })
-                    .catch((error) => {
-                      console.log('errorerror',error)
-                      res.status(error.statusCode).send(error.error.error_description);
-                    });                    
-                  });
-                  // res.json(shopResponse);
-                })
-                .catch((error) => {
-                  console.log('errorerror',error)
+                      request.post(options3)
+                      .then((shopResponse3) => {
+                        console.log('shopResponse3',shopResponse3);
+                        res.redirect(shopResponse1.body.recurring_application_charge.confirmation_url); 
+                        // res.json(shopResponse3);
+                      })
+                      .catch((error) => {
+                        console.log('errorerror',error)
+                        res.status(error.statusCode).send(error.error.error_description);
+                      });                    
+                    });
+                    // res.json(shopResponse);
+                  })
+                  .catch((error) => {
+                    console.log('errorerror',error)
+                });
+
               });
-
+              // res.json(shopResponse);
+            })
+            .catch((error) => {
+              console.log('errorerror',error)
+              res.status(error.statusCode).send(error.error.error_description);
             });
-            // res.json(shopResponse);
-          })
-          .catch((error) => {
-            console.log('errorerror',error)
-            res.status(error.statusCode).send(error.error.error_description);
-          });
-      }); 
+        });
+
+      });       
     })
     .catch((error) => {
       res.status(error.statusCode).send(error.error.error_description);
